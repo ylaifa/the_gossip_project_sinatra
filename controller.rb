13fdbb5bca9c63662eacdf403e228c4e714eb88f
Yousri LAÏFA <laifayousri@gmail.com>
2021-06-22 18:45:57 +0200

    edit controller.rb

--- controller.rb
+++ controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < Sinatra::Base
   get '/' do
-    "<html>
-        <head>
-            <title>The Gossip Project</title>
-        </head>
-        <body>
-            <h1>My great Gossip application !</h1>
-        </body>
-    </html>"
+    erb :index
   end
 end
