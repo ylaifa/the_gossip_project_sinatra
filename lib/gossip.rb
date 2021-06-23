12a80b44fe0f79258d913f1dc776c178a7b09e14
Yousri LAÏFA <laifayousri@gmail.com>
2021-06-23 15:39:42 +0200

    edit gossip.rb add method all and attr_accessor

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,6 +1,8 @@
 require 'csv'
 
 class Gossip 
+  attr_accessor :author, :content
+  
   def initialize(author, content)
     @author = author
     @content = content
@@ -10,6 +12,14 @@ class Gossip
     CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
+  end
+
+  def self.all
+    all_gossips = Array.new
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    all_gossips
   end
 end
 
