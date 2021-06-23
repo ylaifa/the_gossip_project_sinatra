b9c2d5beac3c304c12f8e9ab70b6c2d4d8bc8cdf
Yousri LAÏFA <laifayousri@gmail.com>
2021-06-23 15:13:33 +0200

    edit controller.rb add method post

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,5 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
@@ -5,5 +7,10 @@ class ApplicationController < Sinatra::Base
 
   get '/gossips/new/' do
     erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
   end
 end
